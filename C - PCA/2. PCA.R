c64a0732dd8db6135048bc8000b8a706621825e6
SmithWillJ <158510007+SmithWillJ@users.noreply.github.com>
2025-07-24 16:34:09 +0100

    Update 2. PCA.R

--- C - PCA/2. PCA.R
+++ C - PCA/2. PCA.R
@@ -1,6 +1,7 @@
-setwd("/file/path/")
+setwd("/file/path")
 
 library(ggplot2)
+library(tidyverse)
 
 ## Read in the PCA data
 
@@ -9,14 +10,14 @@ pca_eigenvals <- read.table("PCA_PLINKPRUNED.eigenval")
 location_data <- read.table("GeneFlow_popmap", header = FALSE)
 location_column <- location_data[, 2]
 pca_data <- cbind(pca_data, Location = location_column) %>%
-  unite('Selected', V1:V2, sep = '_')
+  rename(Selected = V1)
 
 ### Plot PC1 vs PC2
 
-sampleinfo <- read_csv("/file/path/to/sample_info4.csv")
+sampleinfo <- read_delim("/file/path/to/sample_info_GeneFlow.csv", delim = ';')
 
 LatitudeInds <- pca_data %>% 
-  left_join(sampleinfo %>% dplyr::select(Selected, lat), by = 'Selected') %>%
+  left_join(sampleinfo %>% mutate(Selected = gsub('_', '', Selected)) %>% dplyr::select(Selected, lat), by = 'Selected') %>%
   arrange(lat)
 
 Latcolours <- data.frame(Location = factor(unique(LatitudeInds$Location), levels = unique(LatitudeInds$Location[order(-LatitudeInds$lat)])),
@@ -31,7 +32,7 @@ color_map <- setNames(pca_data_plot$colrs, pca_data_plot$Location)
 pca_plot1 <- ggplot(pca_data_plot, aes(V3, V4, colour = Location)) +
   geom_point(size = 5) +
   ggtitle("PC1 vs PC2") +
-  xlab("PC1 (2.13%)") +
+  xlab("PC1 (2.12%)") +
   ylab("PC2 (1.70%)") +
   scale_colour_manual(values = color_map)
 pca_plot1
@@ -51,33 +52,35 @@ ggsave('PCA_plot.png', plot = Q, path = '~/Desktop', dpi = 600, width = 13, heig
 
 Q
 
-### Plot PC1 vs PC3
+#### Plot an identical PCA but with colours of points representing parasitism status.
 
-pca_plot2 <- ggplot(pca_data, aes(V3, V5, colour = Location)) + geom_point(size = 2) + ggtitle("PC1 vs PC3") +
-  xlab("PC1 (2.13%)") + ylab("PC3 (1.64%)") +
-  scale_colour_manual(values = c('coral', 'chocolate', 'darkorange2', 'burlywood', 'violet',
-                                 'purple', 'salmon', 'darkblue', 'blue', 'lightblue', 'brown', 'brown1',
-                                 'orange', 'pink', 'deeppink2', 'darkgrey', 'darkgreen', 'aquamarine',
-                                 'lightgreen', 'black', 'green', 'red'))
+ParasitePCA_Data <- sampleinfo %>% dplyr::select(Selected, parasit_yes) %>% 
+  mutate(parasit_yes = as.character(parasit_yes)) %>%
+  mutate(Selected = gsub('_', '', Selected)) %>%
+  right_join(pca_data_plot, by = 'Selected')
 
-pca_plot2
+pca_plot_Parasit <- ggplot(ParasitePCA_Data, aes(V3, V4, colour = as.character(parasit_yes))) +
+  geom_point(size = 5) +
+  ggtitle("PC1 vs PC2") +
+  xlab("PC1 (2.12%)") +
+  ylab("PC2 (1.70%)") + scale_colour_manual(values = c("blue","red", "grey"))
 
-pca_plot2 + geom_text(aes(label = Location))
+pca_plot_Parasit
 
-R <- pca_plot2 + theme(panel.background = element_blank(), panel.grid = element_blank(),
-                  panel.border = element_rect(colour = "black", fill = NA, size = 2),
-                  axis.title = element_text(size=20, face = 'bold'),
-                  axis.text = element_text(size = 15),
-                  legend.text = element_text(size = 15),
-                  legend.key.size = unit(2.5,"line"),
-                  legend.position = "right", legend.title = element_blank(),
-                  legend.key = element_rect(fill = NA),
-                  legend.background = element_rect(colour="transparent", fill = "transparent"))
+pca_plot_Parasit + geom_text(aes(label = parasit_yes))
 
-### Plot the two figures together
+Parasite<- pca_plot_Parasit + 
+  theme(panel.background = element_blank(), 
+        panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", 
+                                    fill = NA, size = 2),
+        axis.title = element_text(size=20, face = 'bold'),
+        axis.text = element_text(size = 15),
+        legend.position = 'none')
+        
+ggsave('PCA_plot.png', plot = Parasite, path = '~/Desktop', dpi = 600, width = 8, height = 10, unit = 'in')
 
-library(gridExtra)
-grid.arrange(Q,R, ncol = 2, widths = c(1,1.5))
+Parasite
 
 ### Plot a map of sites over Europe:
 
@@ -91,18 +94,23 @@ map_info <- read_csv('~/file/path/to/map_info.csv') %>%
   mutate(PARASIT_YES = ifelse(PARASIT_YES == 'Y?', NA, PARASIT_YES)) %>%
   mutate(parasite = ifelse(PARASIT == 0, 'No', 'Yes'))
 
-europe <- st_read('~/file/path/to/ne_50m_land.shp') %>%
+europe <- st_read('~/file/path/to/ne_50m_land/ne_50m_land.shp') %>%
   st_crop(xmin = -10, ymin = 33.151691,
           xmax = 40, ymax = 63) 
 
+Obj <- read_sf('/file/path/to/RW_map.gpkg') %>%
+  st_crop(xmin = -10, ymin = 33.151691,
+          xmax = 40, ymax = 63)
+
 Z <- ggplot() +
   geom_sf(data = europe) +
-  geom_sf(data = map_info, aes(col = parasite)) +
-  geom_sf_text(data = map_info, aes(label = SITE)) +
-  scale_color_manual(values = c("red","blue","black"),
+  geom_sf(data=Obj, alpha = 0.2, fill = 'lightgreen') +
+  geom_sf(data = map_info, aes(col = parasite), size = 5) +
+  scale_color_manual(values = c("blue","red","black"),
                      labels = c("Unparasitised","Parasitised","Unknown")) + theme_bw() +
   theme(panel.grid = element_blank(),panel.border = element_rect(colour = "black", fill = NA, size = 2),
         axis.text = element_text(size = 11),
         legend.position = 'top', legend.title = element_blank())
 
-Z
+ggsave('map.png', Z, path = '~/Desktop')
+
