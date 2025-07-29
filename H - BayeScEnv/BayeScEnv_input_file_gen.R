0d1a7a35941478e1432f1b932647295c0d4ffe7a
SmithWillJ <158510007+SmithWillJ@users.noreply.github.com>
2025-07-29 12:15:05 +0100

    Update BayeScEnv_input_file_gen.R

--- H - BayeScEnv/BayeScEnv_input_file_gen.R
+++ H - BayeScEnv/BayeScEnv_input_file_gen.R
@@ -18,6 +18,6 @@ summary(hierfstat)
 out <- write.bayescan(dat=hierfstat,diploid = TRUE, fn='test.bsc')
 
 #### What about the env data (i.e. parasitism rate)?
-d <- data.frame(data = c(14.6, 14.5, 8.3, 8.1, 0, 21.1, 0, 0, 0, 0, 0, 0, 0, 0, 14.3, 0, 5.8, 0))
+d <- data.frame(data = c(14.6, 14.5, 8.3, 8.1, 0, 21.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14.3, 0, 5.8, 0, 0))
 d$data <- d$data/sd(d$data)
 write.table(d, file = '~/Desktop/test.txt', sep = " ", col.names = F, row.names = F)
